d445cf372d97d1a9ad2551bcbedaa2ae4148b697
Almaz Tazeev <tyj5resd@gmail.com>
2022-06-12 01:55:53 +0300

    crop image

--- aDisk/user_profile/utils/crop_image.py
+++ aDisk/user_profile/utils/crop_image.py
@@ -3,14 +3,12 @@ from .compressor import compressor
 
 def crop_image(src, crop, id):
     try:
-        print(crop)
         res = compressor(src, id)
         if res['status']:
             im = Image.open(res['result_path'])
             im = im.crop((crop['x'], crop['y'], crop['x'] + crop['width'], crop['y'] + crop['height']))
             im.save(res['result_path'])
             return {'status': True, 'name': res['name']}
-        print('compressor error')
         return {'status': False}
     except Exception as e:
         print(e)
