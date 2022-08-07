ea15c19dda71f9a64fd33b33b3b2d0391f0b5c5e
Almaz Tazeev <tyj5resd@gmail.com>
2022-08-07 22:58:29 +0300

    ...

--- client/src/components/DashboardComponents/MyDiskComponents/MDDesktopComponents/MDDCreateWindow/index.jsx
+++ client/src/components/DashboardComponents/MyDiskComponents/MDDesktopComponents/MDDCreateWindow/index.jsx
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import './style.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
 
+import SharedSwitcher from '../../SharedSwitcher'
 
 const MDDCreateWindow = (props) => {
 
+    const [sharedOn, setSharedOn] = useState(false)
+    
     return (
         <>
         <div className="mddcu_bgq" style={props.createOpened ? {display: 'flex'} : {display: 'none'}} onClick={() => props.setCreateOpened(false)}/>
-        <div className="mddcu_container" style={props.createOpened ? {display: 'flex'} : {display: 'none'}}>
-
+        <div className="mddc_container" style={props.createOpened ? {display: 'flex'} : {display: 'none'}}>
+            <div className="shared-access-wrapper-c">
+                <p className="saw-name">
+                    Shared access
+                </p>
+                <div className="saw-data">
+                    <p className="chioce-saw">Off</p>
+                    <SharedSwitcher sharedOn={sharedOn} setSharedOn={setSharedOn}/>
+                    <p className="chioce-saw">On</p>
+                </div>
+            </div>
+            <span className="custom_hr"></span>
+            <div className="mddcu_line">
+                <div className="mddcu_inline-item">
+                    <div className="mddcu_icon">
+                        <FontAwesomeIcon className="icon" icon={faFolder}/>
+                        <p>Folder</p>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     );
--- client/src/components/DashboardComponents/MyDiskComponents/MDDesktopComponents/MDDUploadWindow/index.jsx
+++ client/src/components/DashboardComponents/MyDiskComponents/MDDesktopComponents/MDDUploadWindow/index.jsx
@@ -1,14 +1,78 @@
-import React from "react"
+import React, { useState, useCallback } from "react"
 import './style.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBoxesPacking, faFile } from '@fortawesome/free-solid-svg-icons'
+import { useDropzone } from "react-dropzone";
 
+import SharedSwitcher from '../../SharedSwitcher'
+import { useEffect } from "react";
 
 const MDDUploadWindow = (props) => {
 
+    const [sharedOn, setSharedOn] = useState(false)
+
+    const [resultFiles, setResultFiles] = useState([])
+
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({});
+
+    useEffect(() => {
+        setResultFiles(acceptedFiles)
+    }, [acceptedFiles])
+
+    useEffect(() => {
+        console.log(resultFiles)
+    }, [resultFiles])
+        
     return (
         <>
         <div className="mddcu_bgq" style={props.uploadOpened ? {display: 'flex'} : {display: 'none'}} onClick={() => props.setUploadOpened(false)}/>
-        <div className="mddcu_container" style={props.uploadOpened ? {display: 'flex'} : {display: 'none'}}>
-            <input type='file'></input>
+        <div className="mddu_container" style={props.uploadOpened ? {display: 'flex'} : {display: 'none'}}>
+            <div className="shared-access-wrapper-u">
+                <p className="saw-name">
+                    Shared access
+                </p>
+                <div className="saw-data">
+                    <p className="chioce-saw">Off</p>
+                    <SharedSwitcher sharedOn={sharedOn} setSharedOn={setSharedOn}/>
+                    <p className="chioce-saw">On</p>
+                </div>
+            </div>
+            <span className="custom_hr"></span>
+            <div className="mddcu_line">
+                <div className="mddcu_inline-item">
+                    <div className="mddcu_icon">
+                        <FontAwesomeIcon className="icon" icon={faFile}/>
+                        <p>File</p>
+                    </div>
+                </div>
+                <div className="mddcu_inline-item">
+                    <div className="mddcu_icon">
+                        <FontAwesomeIcon className="icon" icon={faBoxesPacking}/>
+                        <p>Archive as folder</p>
+                    </div>
+                </div>
+            </div>
+            <div {...getRootProps({ className: "drag-and-drop" })} style={isDragActive ? {border: 'dotted 2px rgb(52, 220, 52)'} : {}}>
+                <input className="input-zone" {...getInputProps()} />
+                <div className="dad-desc">
+                {isDragActive ? (
+                    <p>
+                        Release to drop the files here
+                    </p>
+                ) :
+                    <>
+                    <p>Or drop them here</p>
+                    <div className="dad-icon-container">
+                        <FontAwesomeIcon className="icon" icon={faFile}/>
+                        <FontAwesomeIcon className="icon" icon={faBoxesPacking}/>
+                    </div>
+                    <div className="dad-counter">
+                        <p style={{color: 'rgb(52, 220, 52)'}}>{acceptedFiles.length}</p> files added
+                    </div>
+                    </>
+                }
+                </div>
+            </div>
         </div>
         </>
     );
