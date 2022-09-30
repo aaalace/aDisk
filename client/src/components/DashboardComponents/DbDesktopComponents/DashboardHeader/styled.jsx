aa49e72fd9d43ed50a723881cc17196e48f33e39
Almaz Tazeev <tyj5resd@gmail.com>
2022-09-30 20:29:37 +0300

    ...

---
+++ client/src/components/DashboardComponents/DbDesktopComponents/DashboardHeader/styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { HomePageDevices } from '../../../../lib/devices';
+
+export const DashboardSearchInput = styled.input`
+
+    @media ${HomePageDevices.desktop} {
+        
+    }
+
+    @media ${HomePageDevices.tablet} {
+        
+    }
+
+    @media ${HomePageDevices.mobile} {
+        display: none;
+    }
+`
