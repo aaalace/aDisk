c654c8ecf2b1e4a9da8af7579c40a7bfa0213d07
Almaz Tazeev <tyj5resd@gmail.com>
2023-09-04 23:04:14 +0300

    re

--- client/src/components/DashboardComponents/DashboardMyDisk/index.jsx
+++ client/src/components/DashboardComponents/DashboardMyDisk/index.jsx
@@ -30,29 +30,29 @@ const DashboardMyDisk = (props) => {
     }
 
     useEffect(() => {
-        if(props.user_id !== 0){
-            if(props.page === 'shared'){
-                if(!props.items['shared'].filled){
+        if (props.user_id !== 0) {
+            if (props.page === 'shared') {
+                if (!props.items['shared'].filled) {
                     setFolders([])
                     setFiles([])
-                    getItems() 
-                }else{
+                    getItems()
+                } else {
                     setFiles(props.items['shared'].files)
                     setFolders(props.items['shared'].folders)
                 }
             }
-            if(props.page !== 'shared'){
-                if(!props.items['private'].filled){
+            if (props.page !== 'shared') {
+                if (!props.items['private'].filled) {
                     setFolders([])
                     setFiles([])
                     getItems()
-                }else{
+                } else {
                     setFiles(props.items['private'].files)
                     setFolders(props.items['private'].folders)
                 }
             }
         }
-        document.getElementById('no_right_click').addEventListener('contextmenu', (e)=>{e.preventDefault()});
+        document.getElementById('no_right_click').addEventListener('contextmenu', (e) => { e.preventDefault() });
     }, [props.page, props.items, readyToUpdate])
 
     const closeOpenedFolder = () => {
@@ -61,40 +61,40 @@ const DashboardMyDisk = (props) => {
     }
 
     useEffect(() => {
-        if(props.user_id !== 0){
+        if (props.user_id !== 0) {
             setReadyToUpdate(true)
         }
     }, [props.user_id])
 
     useEffect(() => {
-        if(openedFolder){
+        if (openedFolder) {
             getFolderItems()
         }
     }, [openedFolder])
 
     return (
         <div className="dashboard-content" id="no_right_click">
-            <MDMCreateUpload/>
+            <MDMCreateUpload />
             {openedFolder ?
                 <>
-                <div className="ofc_bgq" onClick={() => closeOpenedFolder()}/>
-                <div className="opened-folder-container">
-                    {folderFiles.length > 0 ? folderFiles.map(item => {
-                            return <DashboardItem folderFile={true} key={item.name} item={item} page={props.page} parentFolder={openedFolder} user_id={props.user_id}/>
-                    }) :
-                    <p style={{color: 'var(--black_to_white)'}}>Empty folder</p>}
-                </div> 
+                    <div className="ofc_bgq" onClick={() => closeOpenedFolder()} />
+                    <div className="opened-folder-container">
+                        {folderFiles.length > 0 ? folderFiles.map(item => {
+                            return <DashboardItem folderFile={true} key={item.name} item={item} page={props.page} parentFolder={openedFolder} user_id={props.user_id} />
+                        }) :
+                            <p style={{ color: 'var(--black_to_white)' }}>Empty folder</p>}
+                    </div>
                 </>
-            :
+                :
                 <></>
             }
             <DashboardItemsContainerStyled cnt={folders.length + files.length}>
-                    {folders.map(item => {
-                            return <DashboardItem setOpenedFolderName={setOpenedFolderName} key={item.name} item={item} page={props.page} user_id={props.user_id}/>
-                    })}
-                    {files.map(item => {
-                            return <DashboardItem key={item.name} item={item} page={props.page} user_id={props.user_id}/>
-                    })}
+                {folders.map(item => {
+                    return <DashboardItem setOpenedFolderName={setOpenedFolderName} key={item.name} item={item} page={props.page} user_id={props.user_id} />
+                })}
+                {files.map(item => {
+                    return <DashboardItem key={item.name} item={item} page={props.page} user_id={props.user_id} />
+                })}
             </DashboardItemsContainerStyled>
         </div >
     );
@@ -109,6 +109,6 @@ const mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps, {getFiles, getFolderFiles})(DashboardMyDisk)
+export default connect(mapStateToProps, { getFiles, getFolderFiles })(DashboardMyDisk)
 
 
